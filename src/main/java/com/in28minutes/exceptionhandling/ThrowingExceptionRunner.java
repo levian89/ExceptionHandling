ba656001180b1fe06c4cd1593e97c619c8b6f4b9
levian89 <levi.gabrian@gmail.com>
2023-04-29 15:18:23 +0300

    Checked exception - Currencies don't match (Exception)

--- src/main/java/com/in28minutes/exceptionhandling/ThrowingExceptionRunner.java
+++ src/main/java/com/in28minutes/exceptionhandling/ThrowingExceptionRunner.java
@@ -9,9 +9,9 @@ class Amount {
         this.amount = amount;
     }
 
-    public void add(Amount that) {
+    public void add(Amount that) throws Exception {
         if(!this.currency.equals(that.currency)) {
-            throw new RuntimeException("Currencies Don't Match " + this.currency + " & " + that.currency);
+            throw new Exception("Currencies Don't Match " + this.currency + " & " + that.currency);
         }
             this.amount = this.amount + that.amount;
     }
@@ -22,7 +22,7 @@ class Amount {
 }
 
 public class ThrowingExceptionRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Amount amount1 = new Amount("USD", 10);
         Amount amount2 = new Amount("EUR", 20);
         amount1.add(amount2);
